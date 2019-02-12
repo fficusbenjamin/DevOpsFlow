60d3414bf554e426578e56353929aaa9f5406569
Lech Psztyk <lech.psztyk@gmail.com>
2019-02-12 00:07:32 +0000

    Country Report and testing Methods, tested works

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -8,10 +8,14 @@ public class App
     {
         System.out.println("Starting application, it can take some time. ~20sec");
 
-        //Only for testing purposes, the structure of classes will change in the future
+       // Only for testing purposes, the structure of classes will change in the future
+        Report testCountryReport = new CountryReport();
         Report testReport = new CityReport();
         Report testReportCap = new CapitalReport();
         Report testReportPop = new PopulationReport();
+
+        testCountryReport.loadReport();
+        testCountryReport.displayReport();
         testReportCap.loadReport();
         testReportCap.displayReport();
         testReport.loadReport();
