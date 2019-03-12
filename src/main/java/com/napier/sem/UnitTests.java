c9417821ae6c9e7e8eb848d5a22d5d3670fc3eea
Francesco Fico <fficusbenjamin@gmail.com>
2019-03-12 11:09:34 +0000

    Travis try

--- src/main/java/com/napier/sem/UnitTests.java
+++ src/main/java/com/napier/sem/UnitTests.java
@@ -1,27 +1,33 @@
-package Tests;
-
+package com.napier.sem;
 
 import BusinessLayer.*;
 import BusinessLayer.DataRow.*;
 import com.napier.sem.*;
 import DataLayer.*;
+
+import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.TestInstance;
 
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppIntegrationTest
-{
+
+
+
+public class UnitTests {
+
     static DB_Connection con;
     static DataRow dataRow;
     static CapitalCityRow capitalCityRow;
     static CityRow cityRow;
     static CountryRow countryRow;
+    static Menu mainmenu;
 
 
     // Database tests
@@ -32,32 +38,32 @@ public class AppIntegrationTest
     }
 
     //Data structure tests
-    @org.junit.Test
+    @Test
     public void setDataRow(){
         dataRow = new DataRow();
         assertNotNull(dataRow);
     }
 
-    @org.junit.Test
+    @Test
     public void setCapitalCityRow(){
         capitalCityRow = new CapitalCityRow();
         assertNotNull(capitalCityRow);
     }
 
-    @org.junit.Test
+    @Test
     public void setCityRow(){
         cityRow = new CityRow();
         assertNotNull(cityRow);
     }
 
-    @org.junit.Test
+    @Test
     public void setCountryRow(){
         countryRow = new CountryRow();
         assertNotNull(countryRow);
     }
 
 
-    @org.junit.Test
+    @Test
     public void setCon()
     {
         init();
@@ -76,9 +82,29 @@ public class AppIntegrationTest
         System.out.println(String.format("Timed out after: %1$d ms", System.currentTimeMillis() - startAt));
         System.out.println("Could not connect to the database");
     }
-    @Test(timeout = 30000)
+    /*@Test(timeout = 30000)
     public void setConnection() throws InterruptedException {
         init();
         con.Connect();
+    }*/
+
+    //Menu tests
+    @Test
+    public void setMainmenu()
+    {
+        mainmenu = new Menu("Main Menu");
+        assertNotNull(mainmenu);
     }
+    @Test
+    public void createMenuSystem()
+    {
+        mainmenu = Menu.createMenuSystem();
+        assertNotNull(mainmenu);
+        String menuDisplay = mainmenu.toString();
+        assertNotEquals(menuDisplay.length(),0);
+    }
+
+
+
 }
+
